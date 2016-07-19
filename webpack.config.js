41c044c2cc2db8e7bd51a7389797607d285f3dc5
Park Hyunjeong <anyflow@gmail.com>
2016-07-20 07:45:25 +0900

    webpack-dev-server settings added

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 var webpack = require('webpack');
-var plugins = [];
+var plugins = [
+  new webpack.HotModuleReplacementPlugin()
+];
 
 if(process.env.WEBPACK_ENV === 'build') {
   plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
 }
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080', //FOR HotModuleReplacement
+    'webpack/hot/only-dev-server',                     //FOR HotModuleReplacement
+    './src/index.jsx'
+  ], 
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js',
     publicPath: '/'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx']                    //FOR omiting extension in import statement.
   },
   module: {
     loaders: [
       {
         test: /(\.jsx|.js)$/,
-        loader: 'react-hot-loader!babel',
+        loader: 'react-hot!babel',                     //FOR ES6(babel) and react(react-hot) reloading
         exclude: /node_modules/
       }      
     ]
+  },
+  devServer: {
+    contentBase: './dist',                             //SET content base of webpack-dev-server 
+    hot: true                                          //FOR react-hot-loader
   },
   plugins: plugins
 };
