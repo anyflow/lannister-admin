925fd1059b18bb7797be1122d82d6128aa0a9b56
Park Hyunjeong <anyflow@gmail.com>
2016-07-26 04:31:38 +0900

    react-bootstrap added

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classNames from 'classnames';
+import { Button } from 'react-bootstrap';
 
 class Hello extends React.Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class Hello extends React.Component {
         Type your name : 
         <input onChange={(event) => this.props.setName(event.target.value)} />
         <h1 className={classNames('output')}>Hello { this.props.name }!</h1>
+        <Button>Button React-Bootstrap Sample</Button>
       </div>
     );
   }
